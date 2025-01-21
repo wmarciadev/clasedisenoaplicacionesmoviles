691680968b3dda41c141899d08b6c9b69872d039
wmarciadev <WJMarcia@telcor.gob.ni>
2025-01-21 01:35:10 -0600

    Actualización: descripción breve de los cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.demopizzaapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.demopizzaapp"
